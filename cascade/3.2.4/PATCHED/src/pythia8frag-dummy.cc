aee0ab910dae13c5454d21fefa3e279a596037f4
andriish <andriish@users.noreply.github.com>
2022-10-06 15:11:27 +0200

    Cascade (#232)
    Co-authored-by: Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>

---
+++ cascade/3.2.4/PATCHED/src/pythia8frag-dummy.cc
@@ -0,0 +1,29 @@
+using namespace std;
+
+
+#include <string>
+#include <iostream>
+
+
+
+extern "C" {   
+
+int hepmc_convert_eventp8_(const int & position) {
+    
+   cout << " pythia8frag dummy " << endl;
+      
+
+   }
+int ncountp8 =0 ;
+
+void pythia8frag_(){
+    
+   cout << " pythia8frag dummy " << endl;
+      
+
+   }
+void convhepmcp8_(int & ievent, int & iproc, double & weight, double & xsec, double & xsece, int & id1pdf, int & id2pdf, double & x1pdf, double & x2pdf, double & QFac, double & pdf1, double & pdf2 ){
+   cout << " convhepmcp8 dummy " << endl;
+}
+   
+}
