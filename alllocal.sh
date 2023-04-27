28844d181cc983cceeaa6115332768215880ceae
andriish <andriish@users.noreply.github.com>
2023-04-27 15:23:39 +0200

    Iminuit (#247)
    Co-authored-by: Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>

--- alllocal.sh
+++ alllocal.sh
@@ -4,7 +4,7 @@ export PATH=$PATH:$(pwd)
 
 set -x 
 declare -a BUILDLIST=(
-applgrid:1.6.32
+#applgrid:1.6.32
 #MG5_aMC:3.4.2
 #gosam:2.1.1
 #qd:2.3.23
@@ -35,7 +35,7 @@ applgrid:1.6.32
 #cascade:3.3.0
 #Rivet:3.1.7
 #Herwig:7.2.3
-#iminuit:2.16.0
+iminuit:2.21.3
 #cascade:3.2.1
 #qgraf:3.4.2
 #YODA:1.9.7
