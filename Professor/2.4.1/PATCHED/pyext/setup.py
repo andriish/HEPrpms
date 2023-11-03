32acf52b847140bec7332d994223edda76cf03d1
andriish <andriish@users.noreply.github.com>
2023-11-03 23:04:55 +0100

    Update (#264)
    Co-authored-by: Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>

---
+++ Professor/2.4.1/PATCHED/pyext/setup.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+from setuptools import setup, Extension
+from glob import glob
+
+import os
+srcdir = os.path.relpath("./")
+libdir = os.path.relpath("../lib")
+incdir = os.path.relpath("../include")
+os.environ.setdefault("CC", "g++")
+os.environ.setdefault("CXX", "g++")
+
+# print("****", srcdir)
+# print("****", libdir)
+# print("****", incdir)
+
+ext = Extension("professor2.core",
+                ["professor2/core.cpp"],
+                language="C++",
+                depends=glob("../include/*.h"),
+                include_dirs=[incdir, os.path.join(srcdir, "pyext", "professor2","../include", "../", "./")],
+                extra_compile_args="-I. -I./include -I../ -I../include -std=c++11 -O3 -Wno-unused-but-set-variable -Wno-sign-compare".split(),
+                library_dirs=[libdir],
+                runtime_library_dirs=[],
+                libraries=["Professor2"])
+
+v = os.environ.get("PROF_VERSION", "2.4.1")
+setup(name = "professor2",
+      version=v,
+      ext_modules = [ext],
+      packages = ["professor2", "professor2/ml", "professor2/misc"],
+      description="Professor version 2",
+      author="Professor collaboration",
+      author_email="andy.buckley@cern.ch",
+      url="https://gitlab.com/hepcedar/professor")
