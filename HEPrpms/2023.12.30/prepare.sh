64c0a5b78e340fff98d34b70a1432272cf3197a5
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-12-30 21:31:42 +0100

    Update

--- HEPrpms/2023.12.30/prepare.sh
+++ HEPrpms/2023.12.30/prepare.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 yum --disablerepo="*" --enablerepo=copr:copr.fedorainfracloud.org:averbyts:HEPrpms list available | grep copr: | tr -s ' ' | tr ' ' ':' > 1.txt
+yum list installed | grep copr:copr.fedorainfracloud.org:averbyts:HEPrpms | tr -s ' ' | tr ' ' ':' >> 1.txt
 
-(for a in $(cat 1.txt | grep -v '.src'); do 
+(for a in $(cat 1.txt | grep -v \.src | sort ); do 
  v=$(echo $a| cut -f 2 -d: | cut -f 1 -d\-); 
  p=$(echo $a| cut -f 1 -d: | cut -f 1 -d.); 
  echo $p':'$v; 
