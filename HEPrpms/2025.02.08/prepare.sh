95a94962dca42aac603905b0be4414a78dbea40d
Andrii Verbytskyi <you@example.com>
2025-02-08 10:52:33 +0100

    Add tnew test

---
+++ HEPrpms/2025.02.08/prepare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+yum --disablerepo="*" --enablerepo=copr:copr.fedorainfracloud.org:averbyts:HEPrpms list available | grep copr: | tr -s ' ' | tr ' ' ':' > 1.txt
+yum list installed | grep copr:copr.fedorainfracloud.org:averbyts:HEPrpms | tr -s ' ' | tr ' ' ':' >> 1.txt
+
+(for a in $(cat 1.txt | grep -v \.src | sort ); do 
+ v=$(echo $a| cut -f 2 -d: | cut -f 1 -d\-); 
+ p=$(echo $a| cut -f 1 -d: | cut -f 1 -d.); 
+ echo $p':'$v; 
+ done) > 2.txt
+ 
+ (for a in $(cat 2.txt | grep -v 'debug'); do 
+ v=$(echo $a| cut -f 2 -d: | cut -f 1 -d\-); 
+ p=$(echo $a| cut -f 1 -d: | cut -f 1 -d.); 
+ echo 'Requires:' $p' == '$v; 
+ done)
+ 
+exit
+L="|XXX |[![Copr build status](https://copr.fedorainfracloud.org/coprs/averbyts/HEPrpms/package/XXX/status_image/last_build.png)](https://copr.fedorainfracloud.org/coprs/averbyts/HEPrpms/package/XXX/)  |[![Copr build status](https://copr.fedorainfracloud.org/coprs/averbyts/HEPrpmsSUSE/package/XXX/status_image/last_build.png)](https://copr.fedorainfracloud.org/coprs/averbyts/HEPrpmsSUSE/package/XXX/)   |"
+
+for a in $(copr list-package-names averbyts/HEPrpms | sort); do echo $(echo $L | sed 's/XXX/'$a'/g') ; done 
